79b0b1eb97c76ba04349e2b6446171bc71f82a17
Mateus Pincho <mateus.oliveira@ee.ufcg.edu.br>
2023-10-22 09:41:07 -0300

    corner detection for camera calibration

--- python/exemples/opencv.py
+++ python/exemples/opencv.py
@@ -22,8 +22,8 @@ print('Program started')
 client = RemoteAPIClient()
 sim = client.getObject('sim')
 
-visionSensorHandle = sim.getObject('/VisionSensor')
-passiveVisionSensorHandle = sim.getObject('/PassiveVisionSensor')
+visionSensorHandle = sim.getObject('/Vision_sensor')
+#passiveVisionSensorHandle = sim.getObject('/PassiveVisionSensor')
 
 # When simulation is not running, ZMQ message handling could be a bit
 # slow, since the idle loop runs at 8 Hz by default. So let's make
@@ -44,11 +44,31 @@ while (t := sim.getSimulationTime()) < 10:
     # and color format is RGB triplets, whereas OpenCV uses BGR:
     img = cv2.flip(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), 0)
 
-    cv2.imshow('', img)
-    cv2.waitKey(1)
-    client.step()  # triggers next simulation step
+    
+    # Harris Corner Detection implemented 
+    gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+    gray_blur = cv2.blur(gray,(6,6))
+    
+    #Input must be a single-channel 8-bit or floating-point image.
+    gray_blur = np.float32(gray_blur)
 
-    cv2.imwrite('image.jpg',img)
+    #Corner Detector
+    dst = cv2.cornerHarris(gray_blur,2,3,0.04)
+    dst = cv2.dilate(dst,None)
+    ret, dst = cv2.threshold(dst,0.01*dst.max(),255,0)
+    dst = np.uint8(dst)
+
+    ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
+    corners = cv2.cornerSubPix(gray,np.float32(centroids),(5,5),(-1,-1),criteria)
+
+    res = np.hstack((centroids,corners))
+    res = np.int0(res)
+    img[res[:,1],res[:,0]]=[0,0,255]
+    img[res[:,3],res[:,2]] = [0,255,0]
+
+    img[dst>0.01*dst.max()]=[0,0,255]
+
 
 sim.stopSimulation()
 
